4fc8ebc7c5f6e5c1be29d4af9be05a1ca6f62c40
rupakchaulagain <rupakchaulagain@gmail.com>
2020-07-24 12:01:26 +0545

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
-import App from './components/App'
-// import Todo from './components/Todo'
+// import App from './components/App'
+import Todo from './components/Todo'
 
 
-render(<App />, document.getElementById('root'));
+render(<Todo />, document.getElementById('root'));
